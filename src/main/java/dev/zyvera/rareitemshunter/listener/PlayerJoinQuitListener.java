85d3554cd2be59fa0eefde02e411667b20bbcccf
Thomas U. <Info@ThomasU.dev>
2026-03-11 14:18:13 +0100

    Add Playerjoin Listener Logic

--- src/main/java/dev/zyvera/rareitemshunter/listener/PlayerJoinQuitListener.java
+++ src/main/java/dev/zyvera/rareitemshunter/listener/PlayerJoinQuitListener.java
@@ -1,7 +1,27 @@
 package dev.zyvera.rareitemshunter.listener;
-import org.bukkit.event.Listener;
 
+import dev.zyvera.rareitemshunter.RareItemsHunter;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 
 public class PlayerJoinQuitListener implements Listener {
 
+    private final RareItemsHunter plugin;
+
+    public PlayerJoinQuitListener(RareItemsHunter plugin) {
+        this.plugin = plugin;
+    }
+
+    @EventHandler
+    public void onJoin(PlayerJoinEvent event) {
+        plugin.getPlayerDataManager().load(event.getPlayer());
+    }
+
+    @EventHandler
+    public void onQuit(PlayerQuitEvent event) {
+        plugin.getPlayerDataManager().save(event.getPlayer());
+        plugin.getPlayerDataManager().unload(event.getPlayer().getUniqueId());
+    }
 }
